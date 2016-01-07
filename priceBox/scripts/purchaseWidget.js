fe14d2d685d55b788e794085f921f592156944ca
vincentywang <vincentywang@gmail.com>
2016-01-07 17:56:47 -0500

    correct display price label

--- priceBox/scripts/purchaseWidget.js
+++ priceBox/scripts/purchaseWidget.js
@@ -13,6 +13,7 @@ var widget = (function(jsonData){
 		originData : null,
 		quality : [],
 		linkMsg : 'https://www.github.com',
+
 		getQualityType : function() {
 			for (var index in this.originData) {
 				if (this.originData.hasOwnProperty(index)) {
@@ -21,32 +22,52 @@ var widget = (function(jsonData){
 			}
 			return this.quality;
 		},
+
 		getTextLinkMsg : function() {
 			return this.linkMsg;
 		},
+
 		getQualityPrice : function(qualityTag) { // SD, HD
-			for (var index in this.originData) {
-				if (this.originData.hasOwnProperty(index)) {
-					if (index === qualityTag) {
-						return this.originData[index];
+			console.log("what is origin data");
+			console.log(this.originData);
+			console.warn(this.originData);
+			if (this.originData) {
+				for (var index in this.originData) {
+					if (this.originData.hasOwnProperty(index)) {
+						if (index === qualityTag) {
+							return this.originData[index];
+						}
 					}
-				}
+				}	
+			} else {
+				console.error("dataSource.originData not set");
 			}
+		},
+
+		getQualityBasicPrice : function(qualityTag) {
+			var qualityTag = qualityTag || 'SD' ;
+			console.log(qualityTag);
+			var aPrice = this.getQualityPrice(qualityTag);
+			console.dir(aPrice);
+			return aPrice[3];
 		}
 	};
 
-	function init (argument) {
-		initBasicHtml();
+	function init (qualityTag) {
+		// initBasicHtml();
+		var qualityTag = qualityTag || 'SD' ;
+		bindEvent(qualityTag);
+		loadData();
 		render();
 		testFun();
 	}
 
 	function testFun () {
-		console.trace(dataSource.quality);
+		// console.trace(this.dataSource.quality);
 	}
 
 	// init html wrapper to set up variables
-	function initBasicHtml (argument) {
+	function initBasicHtml (eleTag) {
 		
 		$purchaseWidget =  $("<div></div>", {"id" : "purchaseWidget"});
 		$qualityTab =      $("<div></div>", {"id" : "qualityTab"});
@@ -56,24 +77,33 @@ var widget = (function(jsonData){
 		$priceLabel =      $('<div></div>', {"id" : "priceLabel"});
 		$payButton =       $('<div></div>', {"id" : "payButton"});
 
-		$purchaseWidget
+		return $purchaseWidget
 			.append($qualityTab)
 			.append($textLink)
 			.append($priceList)
 			.append($ticketCounter)
 			.append($priceLabel)
 			.append($payButton);
-
-		// $purchaseWidget.appendTo($('div#myApp'));
-		$('div#myApp').append($purchaseWidget);
-
 	}
 	
 
-	function render () {
-		loadData();
+	function render (qualityTag) {
+		console.log("what is the qualityTag");
+		console.log(qualityTag);
+		if (typeof qualityTag === 'undefined') {
+			var qualityTag = qualityTag || 'SD' ;
+			var eleTag = 'div#myApp';
+			// var price = this.dataSource.getQualityBasicPrice(qualityTag);
+			// console.log(price);
+			$(eleTag).append(initBasicHtml());
+		} else {
+			var price = dataSource.getQualityBasicPrice(qualityTag);
+			buildPriceList(qualityTag);
+			buildPriceLabel(price);
+		}
+	
 		// buildQualityTab();
-		console.log("call this render function");
+		console.log("render.call");
 	}	
 
 	function loadData () {
@@ -81,15 +111,39 @@ var widget = (function(jsonData){
 			url : 'data.json',
 			dataType : 'json',
 			success : function(data) {
-				console.log(data);
-				dataSource.originData = data;
-				// dataSource.getQualityType();
-				buildQualityTab();
-				buildTextLink();
-				buildPriceList('SD');
-				buildTicketCounter();
+				$(document)
+					.trigger("widget/load/success", data)
+					.trigger("widget/build/qualityTab")
+					.trigger("widget/build/textLink")
+					.trigger("widget/build/priceList")
+					.trigger("widget/build/ticketCounter")
+					.trigger("widget/build/priceLabel");
 			}
 		});
+	}
+
+	function bindEvent (qualityTag) {
+		$(document)
+			.on("widget/load/success", function(event, data){
+				console.log(data);
+				dataSource.originData = data;
+			})
+			.on("widget/build/qualityTab", function() {
+				buildQualityTab();
+			})
+			.on("widget/build/textLink", function() {
+				buildTextLink();
+			})
+			.on("widget/build/priceList", function() {
+				buildPriceList(qualityTag);
+			})
+			.on("widget/build/ticketCounter", function() {
+				buildTicketCounter();
+			})
+			.on("widget/build/priceLabel", function(){
+				var price = dataSource.getQualityBasicPrice();
+				buildPriceLabel(price);
+			});
 	}
 	
 	
@@ -180,6 +234,7 @@ var widget = (function(jsonData){
 	function buildPriceList (qualityTag) {
 
 		var oPriceList = dataSource.getQualityPrice(qualityTag);
+		console.log(oPriceList);
 
 		// var priceList = $('<div/>', {
 		// 	id : "purchaseWidgetLeft",
@@ -219,6 +274,7 @@ var widget = (function(jsonData){
 	 * @return {[type]}
 	 */
 	function initPriceList () {	
+
 		console.log("initPriceList.call");
 		// init price list select the first one
 		$("div.single-price-box").eq(0).addClass('select');
@@ -230,6 +286,7 @@ var widget = (function(jsonData){
 
 	// leason event about user click, update number according to the index
 	function buildTicketCounter () {
+
 		var labelText = "Number of tickets";
 		var labelTextEle = $("<div>", {
 			"class" : "ticket-label-text",
@@ -260,11 +317,12 @@ var widget = (function(jsonData){
 		counter.appendTo($ticketCounter);
 		initPriceList();
 		registerTicketArrows();
+
 	}
 
 	
 	function buildPriceLabel (price) {
-		
+		var price = price || 0 ;
 		var priceLabel = $("<div>", {
 			class : "price-label",
 			html : '<div class="price-label-text"></div><div class="price-wrapper"><div class="price">' + price +'</div></div>'
@@ -303,8 +361,10 @@ var widget = (function(jsonData){
 		qualityTab.on('click', function() {
 			var quality = $(this).data('tag');
 			console.log("clicked " + quality + " tab");
-			buildPriceList(quality);
-			buildPriceLabel('20');
+			// buildPriceList(quality);
+			// buildPriceLabel('20');
+			console.log(quality);
+			render(quality);
 		});
 	}
 
